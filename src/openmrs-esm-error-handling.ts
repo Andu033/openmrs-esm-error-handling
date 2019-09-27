d8318a241b5eb7f985af6bb7ea66ea883e56e5ad
Stanila Andrei <andrei133303@gmail.com>
2019-09-27 13:04:41 +0300

    Conflict

--- src/openmrs-esm-error-handling.ts
+++ src/openmrs-esm-error-handling.ts
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import { showToast } from "@openmrs/esm-styleguide";
-=======
-// import { showToast } from '@openmrs/esm-styleguide'
 import "./set-public-path";
->>>>>>> 91d318763f783771cac27579c1bf08f3093b1cbe
 
 export function handleApiError() {
   return incomingResponseErr => {
